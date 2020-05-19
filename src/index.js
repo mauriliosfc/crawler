69d8ad489106b004c1d9ecc357b516674c3135c3
Maurílio B. Martins <maurilio.martins@tcsindustrial.com.br>
2020-05-19 09:34:47 -0300

    first commit

--- src/index.js
+++ src/index.js
@@ -2,22 +2,23 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const url = "https://g1.globo.com/";
 
-fetchData(url).then((res) => {
-    const html = res.data;
-    const $ = cheerio.load(html);
+fetchData(url)
+    .then((res) => {
+        const html = res.data;
+        const $ = cheerio.load(html);
 
-    const page = $('.bastian-page div div div div');
-    let arr = []
+        const page = $('.bastian-page div div div div');
+        let arr = []
 
-    page.each(function () {
-        let title = $(this).find('.feed-post-body-title').text();
-        let subtitulo = $(this).find('.feed-post-body-resumo').text();
-        let url = $(this).find('.feed-post-body-title').attr('href');
-        if (title.length > 0 && arr.length < 3)
-            arr.push({ title: title, subtitulo: subtitulo, url: url })
-    });
-    console.log(arr)
-})
+        page.each(function () {
+            let title = $(this).find('.feed-post-body-title').text();
+            let subtitulo = $(this).find('.feed-post-body-resumo').text();
+            let url = $(this).find('.feed-post-body-title').attr('href');
+            if (title.length > 0 && arr.length < 3)
+                arr.push({ title: title, subtitulo: subtitulo, url: url })
+        });
+        console.log(arr)
+    })
 
 async function fetchData(url) {
     console.log("Crawling data...")
