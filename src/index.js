aa801b9f8c692f661cdecf0d04945c0c5b746454
Maurílio B. Martins <maurilio.martins@tcsindustrial.com.br>
2020-05-18 23:40:31 -0300

    first commit

---
+++ src/index.js
@@ -0,0 +1,32 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const url = "https://g1.globo.com/";
+
+fetchData(url).then((res) => {
+    const html = res.data;
+    const $ = cheerio.load(html);
+
+    const statsTable = $('.bastian-page div div div div');
+    let arr = []
+
+    statsTable.each(function () {
+        let title = $(this).find('.feed-post-body-title').text();
+        let subtitulo = $(this).find('.feed-post-body-resumo').text();
+        let url = $(this).find('.feed-post-body-title').attr('href');
+        if (title.length > 0 && arr.length < 3)
+            arr.push({ title: title, subtitulo: subtitulo, url: url })
+    });
+    console.log(arr)
+})
+
+async function fetchData(url) {
+    console.log("Crawling data...")
+
+    let response = await axios(url).catch((err) => console.log(err));
+
+    if (response.status !== 200) {
+        console.log("Erro ao buscar dados");
+        return;
+    }
+    return response;
+}
